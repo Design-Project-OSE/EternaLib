193a3d1514ad20fa909328250d04e98414da28f4
EmreYoruk <emreyoruk01@gmail.com>
2024-03-26 18:08:57 +0300

    created login, home, layouts components and coded routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "",
+    component: LayoutsComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "home",
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { LoginFormComponent } from './components/login/login-form/login-form.component';
+import { RegisterFormComponent } from './components/login/register-form/register-form.component';
+import { LoginNavbarComponent } from './components/login/login-navbar/login-navbar.component';
+import { LoginFooterComponent } from './components/login/login-footer/login-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LayoutsComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    LoginNavbarComponent,
+    LoginFooterComponent
   ],
   imports: [
     BrowserModule,
