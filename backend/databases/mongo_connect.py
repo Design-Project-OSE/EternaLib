dd1c4a4ab9567a0ea6f8930bec5c7630aa694814
Sinan Uyğun <sinan_uygun@windowslive.com>
2024-03-27 12:53:15 +0300

    testing databases

--- backend/databases/mongo_connect.py
+++ backend/databases/mongo_connect.py
@@ -4,12 +4,16 @@ class databases:
     def __init__(self) -> None:
         pass
 
-    def connect(self,account:str)->list:
+    # database bağlantısı oluşturuyor
+    def connect(self,account:str)->pymongo.MongoClient:
         myclient= pymongo.MongoClient(account)
         return myclient
 
 
-account="mongodb+srv://sinanuygun:BnKJOKx7OTvSWLfD@eternalib.06ijzom.mongodb.net/?retryWrites=true&w=majority&appName=eternaLib"
+accounts="mongodb+srv://sinanuygun:BnKJOKx7OTvSWLfD@eternalib.06ijzom.mongodb.net/?retryWrites=true&w=majority&appName=eternaLib"
+accounte="mongodb+srv://emreyoruk:Mipym9mZgEn3raJm@eternalib.06ijzom.mongodb.net/"
+accounto="mongodb+srv://onurkarakaya:tCPSdNsx6MQWOzOc@eternalib.06ijzom.mongodb.net/"
 db=databases()
-myclient=db.connect(account) 
+myclient=db.connect(accounto) 
+print(accounto)
 print(myclient.list_database_names())
