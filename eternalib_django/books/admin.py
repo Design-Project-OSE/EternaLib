6173fd044074876e564c291857378b4ff74b680b
Sinan Uyğun <sinan_uygun@windowslive.com>
2024-05-08 15:40:53 +0300

    kriptology eklendi

---
+++ eternalib_django/books/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Books_Table,Books_Genres_Table
+
+class BooksAdmin(admin.ModelAdmin):
+    list_display=('id','name','savedate','isPublished')
+    list_display_links=('id','name')
+    list_filter=['savedate']
+    list_editable=('isPublished',)
+    search_fields=('id','name','about','genres')
+    list_per_page=10
+    
+class Books_GenresAdmin(admin.ModelAdmin):
+    list_display=('gen_id','name','savedate')
+    list_display_links=('gen_id','name')
+    list_filter=['savedate']
+    search_fields=('id','name')
+    list_per_page=10
+    
+admin.site.register(Books_Table,BooksAdmin)
+
+admin.site.register(Books_Genres_Table,Books_GenresAdmin)
