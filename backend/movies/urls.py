b62f014adb43490e4b427c630695081947d16e8e
Sinan Uyğun <sinan_uygun@windowslive.com>
2024-05-19 19:12:37 +0300

    hotfix ver0.06

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -13,12 +13,12 @@ http://127.0.0.1:8000/movies/id id:film ID verilen filmi verir
 
 """
 urlpatterns=[
-    path('movies/',views.list_movie,name="Filmler"),
-    path('movie/category/',views.list_moviecategory,name="Film Kategorileri"),
-    path('movies/comment/',views.list_moviecomment,name="Film Yorumları"),
-    path('movies/like/',views.list_movielike,name="Film Beğenileri"),
-    path('movies/id/<uuid:id>/',views.list_movieid,name="Film id sayfası"),
-    path('movies/urlname/<str:urlname>/',views.list_movieurlname,name="Film id sayfası"),
+    path('movies',views.list_movie,name="Filmler"),
+    path('movie/category',views.list_moviecategory,name="Film Kategorileri"),
+    path('movies/comment',views.list_moviecomment,name="Film Yorumları"),
+    path('movies/like',views.list_movielike,name="Film Beğenileri"),
+    path('movies/id/<uuid:id>',views.list_movieid,name="Film id sayfası"),
+    path('movies/urlname/<str:urlname>',views.list_movieurlname,name="Film id sayfası"),
     path('movies/get/comment',views.list_moviegetcomment,name="Yorum yapma"),
     path('movies/get/like',views.list_moviegetlike,name="Beğeni yapma"),
     path('movies/get/id/comment',views.list_getidcomments,name="POST olarak girilen movieID ait tüm yorumları çekiyor"),
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_user, name='register'),
-    path('api/login/', views.user_login, name='user_login'),
+    path('register', views.register_user, name='register'),
+    path('api/login', views.user_login, name='user_login'),
     path('api/profile/<uuid:pk>', views.UserProfileAPIView.as_view(), name='Kullanıcı Profili alma GET'),
     path('api/profile/edit', views.UserProfileUpdateAPIView.as_view, name='Kullanıcı Profili düzenleme POST'),
-    path('api/forgorpass/',views.forgot_password,name="Şifre Unutma"),
-    path('logout/', views.user_logout, name='logout'),
+    path('api/forgorpass',views.forgot_password,name="Şifre Unutma"),
+    path('logout', views.user_logout, name='logout'),
 ]
