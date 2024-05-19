801027992940cfbf2ff3151b2cc42dacfc97a847
emreyoruk0 <emreyoruk01@gmail.com>
2024-05-19 10:32:17 +0300

    trial folder deleted

--- eternalib_django/movies/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('movies/all',views.list_movie,name="movie"),
-    path('movies/category',views.list_categorymovie,name="kategori"),
-]
--- eternalib_django/mss.py
+++
@@ -1,7 +0,0 @@
-import pypyodbc
-connection = pypyodbc.connect('DRIVER={SQL Server};SERVER=SINAN\SQLEXPRESS;DATABASE=kullanıcı;UID=sa;PWD=Şifre')
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM HumanResources.Department")
-sonuc = cursor.fetchall()
-for i in sonuc:
-    print(i)
