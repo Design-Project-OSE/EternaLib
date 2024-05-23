a6070e296c0992fd432e38455834a7941e6848ac
Sinan Uyğun <sinan_uygun@windowslive.com>
2024-05-23 11:23:09 +0300

    hotfix ver0.16

--- backend/account/models.py
+++ backend/account/models.py
@@ -6,7 +6,7 @@ class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,verbose_name="ID")  
     full_name = models.CharField(blank=True, null=True,max_length=255,verbose_name="İsim Soyad")
     email = models.EmailField(blank=True, null=True,unique=True,verbose_name="E-Mail")
-    about=models.CharField(blank=True, null=True,max_length=500,verbose_name="Hakkında",blank=True, null=True,)
+    about=models.CharField(max_length=500,verbose_name="Hakkında",blank=True, null=True,)
     username = models.CharField(max_length=150, unique=True,blank=True, null=True,verbose_name="Kullanıcı Adı")
     password = models.CharField(blank=True, null=True,max_length=128,verbose_name="Şifre")
     profil_picture=models.ImageField(blank=True, null=True,verbose_name="Profil Resmi")
