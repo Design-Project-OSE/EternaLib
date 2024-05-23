8518945fbf83aff7459721a8285cffa04627dfd4
Sinan Uyğun <sinan_uygun@windowslive.com>
2024-05-23 15:53:20 +0300

    hotfix ver0.23

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -9,7 +9,7 @@ views.py içerisinde fonksiyonlar bulunur
 http://127.0.0.1:8000 ana sayfa
 http://127.0.0.1:8000/games/ oyun özellikleri döner
 http://127.0.0.1:8000/games/category/ oyun kategorilerinin bilgilerini verir
-!!!EKLENMEDİ!!!
+
 http://127.0.0.1:8000/games/id id:oyun ID verilen filmi verir
 
 """
